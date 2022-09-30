0d889ba0cc735f396e471d8d7bd86e519d1e1e9d
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2022-09-30 23:52:54 +0700

    Install items now sort in a complicated way Overall sort in a logical order Fix pack install page froze too long

--- DBDIconRepo/ViewModel/PackInstallViewModel.cs
+++ DBDIconRepo/ViewModel/PackInstallViewModel.cs
@@ -1,50 +1,41 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DBDIconRepo.Model;
+using IconInfo.Icon;
+using IconInfo.Internal;
 using IconPack.Model;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Input;
+using System.Xaml;
 
 namespace DBDIconRepo.ViewModel;
 
 public partial class PackInstallViewModel : ObservableObject
 {
-    Pack? _selected;
-    public Pack? SelectedPack
-    {
-        get => _selected;
-        set => SetProperty(ref _selected, value);
-    }
+    [ObservableProperty]
+    Pack? selectedPack;
 
-    ObservableCollection<IPackSelectionItem>? _selections;
-    public ObservableCollection<IPackSelectionItem>? InstallableItems
-    {
-        get => _selections;
-        set => SetProperty(ref _selections, value);
-    }
+    [ObservableProperty]
+    ObservableCollection<IPackSelectionItem>? installableItems;
 
-    ObservableCollection<SelectionMenuItem> _menu;
-    public ObservableCollection<SelectionMenuItem> Menu
-    {
-        get => _menu;
-        set => SetProperty(ref _menu, value);
-    }
+    [ObservableProperty]
+    ObservableCollection<SelectionMenuItem> menu;
 
-    public ICommand? InstallPack { get; private set; }
-    public ICommand? SelectAll { get; private set; }
-    public ICommand? UnSelectAll { get; private set; }
-    public ICommand? SelectSpecifics { get; private set; }
     public PackInstallViewModel() { }
     public PackInstallViewModel(Pack? selected)
     {
-        SelectAll = new RelayCommand<RoutedEventArgs>(SelectAllAction);
-        UnSelectAll = new RelayCommand<RoutedEventArgs>(UnSelectAllAction);
-
         SelectedPack = selected;
-        InstallableItems = new ObservableCollection<IPackSelectionItem>(selected.ContentInfo.Files.Select(path => new PackSelectionFile(path)));
-                    
+        var selections = selected.ContentInfo.Files
+            .Where(file => file.EndsWith(".png") && !file.StartsWith(".banner"))
+            .Select(path => new PackSelectionFile(path))
+            .OrderBy(i => i, new IconComparer());
+        //Sort
+        InstallableItems = new(selections);
+
         //Load selection menu helper
         Menu = new ObservableCollection<SelectionMenuItem>();
         //Load from Json somehow
@@ -59,15 +50,113 @@ public partial class PackInstallViewModel : ObservableObject
         }
     }
 
-    private void UnSelectAllAction(RoutedEventArgs? obj)
+    [RelayCommand]
+    private void UnSelectAll(RoutedEventArgs? obj)
     {
         SetAllItems(false);
     }
 
-    private void SelectAllAction(RoutedEventArgs? obj)
+    [RelayCommand]
+    private void SelectAll(RoutedEventArgs? obj)
     {
         SetAllItems(true);
-    }
+    }    
+}
 
-    
+public class IconComparer : IComparer<IPackSelectionItem>
+{
+    public int Compare(IPackSelectionItem? x, IPackSelectionItem? y)
+    {
+        //Sort by path string
+        if (x.Info is null && y.Info is null)
+            return x.FullPath.CompareTo(y.FullPath);
+
+        //Then by type name
+        if (x.Info.GetType().Name != y.Info.GetType().Name)
+            return x.Info.GetType().Name.CompareTo(y.Info.GetType().Name);
+
+        if (x.Info.GetType().Name == y.Info.GetType().Name)
+        {
+            //Then by indivitual type information
+            switch (x.Info)
+            {
+                case Emblem xEmblem:
+                    var yEmblem = y.Info as Emblem;
+                    //Compare by category first
+                    if (!Equals(xEmblem.Category, yEmblem.Category))
+                    {
+                        //Different category, sort by its
+                        return xEmblem.Category.CompareTo(yEmblem.Category);
+                    }
+                    else
+                        return xEmblem.Quality.CompareTo(yEmblem.Quality); //sort by quality
+                case Addon xad:
+                    var yad = y.Info as Addon;
+                    //Sort by survivor addons
+                    //Then by killer power name
+                    //TODO:Then by rarity??
+                    //Then by name for now
+                    if (xad.Owner is null && yad.Owner is null) //Survivor addons
+                    {
+                        //Survivor item addons
+                        //Sort by addon parent (item) first, then by name
+                        if (Equals(xad.For, yad.For))
+                            return xad.Name.CompareTo(yad.Name);
+                        return xad.For.CompareTo(yad.For);
+                    }                    
+                    if (xad.Owner is not null && yad.Owner is not null) //Killer addons
+                    {
+                        //For now, sort by name only
+                        if (Equals(xad.For, yad.For))
+                            return xad.Name.CompareTo(yad.Name);
+                        return xad.For.CompareTo(yad.For);
+                    }
+                    else if (xad.Owner is null || yad.Owner is null)
+                        return xad.Owner is null ? 1 : -1; //If it doesn't have owner = survivor item addons
+                    break;
+                case Power xPow:
+                    var yPow = y.Info as Power;
+                    if (!Equals(xPow.Owner, xPow.Owner)) //Same owner
+                        return xPow.Owner.CompareTo(yPow.Owner); //Sort by owner
+                    return xPow.Name.CompareTo(yPow.Name); //Then sort by name
+                case Perk xPerk:
+                    var yPerk = y.Info as Perk;
+                    //Sort by Folder (no folder stay at bottom)
+                    //Then by owner name
+                    if ((xPerk.Folder is not null && yPerk.Folder is not null) ||
+                        (xPerk.Folder is null && yPerk.Folder is null)) //Either both of them null or not null
+                    {
+                        //Perk is within folder
+                        if (!Equals(xPerk.Folder, yPerk.Folder)) //Different folder
+                            return xPerk.Folder.CompareTo(yPerk.Folder); //Sort by folder
+                        //Same folder
+                        if (!Equals(xPerk.Owner, yPerk.Owner)) //Different owner
+                            return xPerk.Owner.CompareTo(yPerk.Owner); //Sort by owner
+                        return xPerk.Name.CompareTo(yPerk.Name); //Same owner, sort by name
+                    }
+                    else //One of them is null
+                        return xPerk.Folder is null ? 1 : -1;
+                case Portrait xPor:
+                case Offering xoff:
+                case Item ix:
+                    string? xValue = (string?)x.Info.GetType().GetProperty("Folder").GetValue(x.Info);
+                    string? yValue = (string?)y.Info.GetType().GetProperty("Folder").GetValue(y.Info);
+                    if (xValue is not null && yValue is not null)
+                    {
+                        if (Equals(x.Info.GetType().GetProperty("Folder"), y.Info.GetType().GetProperty("Folder")))
+                            return x.Info.Name.CompareTo(y.Info.Name);
+                        return string.Compare(xValue, yValue);
+                    }
+                    else if (xValue is null && yValue is null)
+                        return x.Info.Name.CompareTo(y.Info.Name);
+                    else
+                        return xValue is null ? 1 : -1;
+                default: //Daily ritual & status effects sort only by name
+                    if (x.Info is UnknownIcon || y.Info is UnknownIcon)
+                        return 0;
+                    return x.Info.Name.CompareTo(y.Info.Name);
+            }
+        }
+        return 0;
+    }
 }
