e6dfa8ef6e5245b833580458e1721d5461e34022
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2022-10-03 02:44:04 +0700

    basic uis

---
+++ DBDIconRepo/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DBDIconRepo.Service;
+
+namespace DBDIconRepo.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+
+    }
+
+    [ObservableProperty]
+    private string currentPageName = "Home";
+
+    public OctokitService GitService => OctokitService.Instance;
+}
---
+++ DBDIconRepo/Views/Home.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DBDIconRepo.Views
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void StartupAction(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SwitchPage(ModernWpf.Controls.NavigationView sender, ModernWpf.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+
+        }
+
+    }
+}
