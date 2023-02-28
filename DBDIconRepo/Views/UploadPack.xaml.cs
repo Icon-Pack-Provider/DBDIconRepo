f287ba140d286fd2dcdb909c5d33c5df5dbddd45
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2023-03-01 03:00:59 +0700

    Slightly revamped upload flow

--- DBDIconRepo/Views/UploadPack.xaml.cs
+++ DBDIconRepo/Views/UploadPack.xaml.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DBDIconRepo.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace DBDIconRepo.Views
+namespace DBDIconRepo.Views;
+
+/// <summary>
+/// Interaction logic for UploadPack.xaml
+/// </summary>
+public partial class UploadPack : Page
 {
-    /// <summary>
-    /// Interaction logic for UploadPack.xaml
-    /// </summary>
-    public partial class UploadPack : Page
+    public UploadPack()
     {
-        public UploadPack()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+}
+
+public class UploadItemTemplator : DataTemplateSelector
+{
+    public DataTemplate UploadableFolderTemplator { get; set; }
+    public DataTemplate UploadableItemTemplator { get; set; }
+
+    public override DataTemplate SelectTemplate(object item, DependencyObject container)
+    {
+        if (item is UploadableFolder)
+            return UploadableFolderTemplator;
+        else
+            return UploadableItemTemplator;
     }
 }
