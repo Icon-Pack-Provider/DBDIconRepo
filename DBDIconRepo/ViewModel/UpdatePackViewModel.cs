2c664c73f1f6ed0d3d55230d2ae6ba735ade7a9b
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2023-03-06 18:14:39 +0700

    ViewModel and paging for update

---
+++ DBDIconRepo/ViewModel/UpdatePackViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DBDIconRepo.Helper;
+using DBDIconRepo.Model;
+using DBDIconRepo.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DBDIconRepo.ViewModel;
+
+public partial class UpdatePackViewModel : ObservableObject
+{
+    public OctokitService Git => Singleton<OctokitService>.Instance;
+    public Setting Config => SettingManager.Instance;
+
+    #region Pages
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(ShowOnSetNewIconsDirectory))]
+    [NotifyPropertyChangedFor(nameof(ShowOnInvalidIconDirectory))]
+    [NotifyPropertyChangedFor(nameof(ShowOnSelectNewIcons))]
+    [NotifyPropertyChangedFor(nameof(ShowOnCommitMessage))]
+    private UpdatePages currentPage = UpdatePages.SetNewIconsDirectory;
+
+    public Visibility ShowOnSetNewIconsDirectory => CurrentPage == UpdatePages.SetNewIconsDirectory ? Visibility.Visible : Visibility.Collapsed;
+    public Visibility ShowOnInvalidIconDirectory => CurrentPage == UpdatePages.InvalidIconDirectory ? Visibility.Visible : Visibility.Collapsed;
+    public Visibility ShowOnSelectNewIcons => CurrentPage == UpdatePages.SelectNewIcons ? Visibility.Visible : Visibility.Collapsed;
+    public Visibility ShowOnCommitMessage => CurrentPage == UpdatePages.CommitMessage ? Visibility.Visible : Visibility.Collapsed;
+
+
+    #endregion
+}
+
+public enum UpdatePages
+{
+    SetNewIconsDirectory,
+    InvalidIconDirectory,
+    SelectNewIcons,
+    CommitMessage
+}
