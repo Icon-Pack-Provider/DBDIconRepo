4a61f8bc9b66c63c3890d546a355887c997d71e9
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2023-03-09 19:42:10 +0700

    Single instance mutex

--- DBDIconRepo/App.xaml.cs
+++ DBDIconRepo/App.xaml.cs
@@ -2,6 +2,10 @@
 using DBDIconRepo.Service;
 using IconPack;
 using SelectionListing;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
 
 namespace DBDIconRepo;
@@ -11,8 +15,43 @@ namespace DBDIconRepo;
 /// </summary>
 public partial class App : Application
 {
+    [DllImport("user32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    private const int SW_RESTORE = 9;
+
+    private const string AppUniqueName = "DBDIconRepository:SpaghettiOfMadness";
+    private bool _isIntantiated;
+    private Mutex? _appMutex;
+
     protected override void OnStartup(StartupEventArgs e)
     {
+        base.OnStartup(e);
+        //Single instance
+        _appMutex = new Mutex(true, AppUniqueName, out _isIntantiated);
+
+        if (!_isIntantiated)
+        {
+            _appMutex = null;
+            // activate existing instance
+            Process current = Process.GetCurrentProcess();
+            foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+            {
+                if (process.Id != current.Id)
+                {
+                    ShowWindow(process.MainWindowHandle, SW_RESTORE);
+                    SetForegroundWindow(process.MainWindowHandle);
+                    break;
+                }
+            }
+            // exit current instance
+            Current.Shutdown();
+            return;
+        }
+        //Git
         OctokitService.Instance.InitializeGit();
         if (OctokitService.Instance.IsAnonymous)
         {
@@ -25,7 +64,19 @@ public partial class App : Application
             Lists.Initialize(OctokitService.Instance.GitHubClientInstance, SettingManager.Instance.CacheAndDisplayDirectory);
         }
         StarService.Instance.InitializeStarService();
-        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+
+        if (_isIntantiated)
+        {
+            _appMutex.ReleaseMutex();
+        }
+
+        _appMutex.Close();
+        _appMutex = null;
     }
 
     public static bool IsDevelopmentBuild()
