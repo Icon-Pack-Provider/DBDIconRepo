0bd26b8eeca326cf790bfdfa97dc50b23fc24c9a
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2023-03-30 01:36:03 +0700

    API Reduction attempt - Cache search result - Limit timeframe for search

---
+++ PackCore/Internal/Helper/APICallRecord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IconPack.Internal.Helper;
+
+public static class APICallRecord
+{
+    //Save the time on last call of SearchRepositoriesRequest
+    const string LastSearchDateRecord = "lastSearchDate.txt";
+    public static void SaveLastSearchDate()
+    {
+        var file = IOHelper.GetFile(LastSearchDateRecord);
+        if (!file.Exists)
+            file.Create();
+        File.WriteAllText(file.FullName, DateTime.Now.ToString("G", CultureInfo.InvariantCulture.DateTimeFormat));
+    }
+
+    public static bool ShouldDoSearch()
+    {
+        var file = IOHelper.GetFile(LastSearchDateRecord);
+        if (!file.Exists)
+            return true;
+        string timeSTR = File.ReadAllText(file.FullName);
+        bool parsed = DateTime.TryParseExact(timeSTR, "G", CultureInfo.InvariantCulture.DateTimeFormat, DateTimeStyles.AssumeUniversal, out DateTime parsedTime);
+        if (!parsed)
+            return true;
+        return parsedTime < (DateTime.UtcNow.AddHours(18));
+    }
+
+    
+}
