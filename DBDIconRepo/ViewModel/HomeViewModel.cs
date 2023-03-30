cfae5aa349ecf88a37c768e64b94c0b8a4bc2f36
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2023-03-30 22:32:00 +0700

    Default icons and uninstall now more easier to find

--- DBDIconRepo/ViewModel/HomeViewModel.cs
+++ DBDIconRepo/ViewModel/HomeViewModel.cs
@@ -468,6 +468,7 @@ public enum PackView
 public class PackDisplayComponentOptions
 {
     public bool ShowFavoriteComponent { get; set; } = true;
+    public bool ShowTopPanelComponent { get; set; } = true;
 
     public PackDisplayComponentOptions() { }
 }
